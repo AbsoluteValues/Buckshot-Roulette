477b245696f1efd89ed7df694e9eb640e0929a3a
Absolute Value <heejun1708@gmail.com>
2025-07-23 17:45:26 +0900

    feat: add BulletTable class

---
+++ index.py
@@ -0,0 +1,25 @@
+class BulletTable() :
+    def __init__(self) :
+        self.bullets = [] # 총알
+        self.live = 0 # 실탄
+        self.blank = 0 # 공포
+
+    def reset(self) :
+        self.bullets.clear()
+        self.live = 0
+        self.blank = 0
+
+    def generate(self) :
+        self.reset()
+
+        amount = random.randrange(2, 8+1)
+
+        for _ in range(amount) :
+            num = random.randrange(0, 1+1)
+            if num == 1 :
+                self.live += 1
+                self.bullets.append("실탄")
+            else :
+                self.blank += 1
+                self.bullets.append("공포탄")
+
