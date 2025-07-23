034dda8792c82dc1d38d5329dc72ab367e1c5bda
YakiMaguro <lvl1n9un7@tukorea.ac.kr>
2025-07-23 22:07:30 +0900

    fix: sldoal

--- index.py
+++ index.py
@@ -42,23 +42,31 @@ class Shotgun() :
         self.bullets.pop(0)
         
 class HandCuffs:             # 수갑 : 상대 턴 제약
+    print("수갑")
 
 class Beer:                  # 맥주 : 현재 장전된 탄약 배출
-    
+    print("맥주")
+
 class MagnifyingGlass:       # 돋보기 : 현재 장전된 탄약 확인
-    
+    print("돋보기")
+
 class Cigarret:              # 담배 : 체력 1 회복
-    
+    print("담배")
+
 class ChainsawTino:          # 톱 : 탄환의 공격력 2배증가
-    
+    print("톱")
+
 class Phone:                 # 대포폰 : 장전된 탄환 외 나머지 탄환중 랜덤으로 몇번째탄약이 실탄인지 알려줌
-    
+    print("폰")
+
 class Inverter:              # 변환기 : 현재 장전된 탄약을 전환 (실탄 <-> 공포탄)
-    
+    print("변환기")
+
 class Adrenaline:            # 아드레날린 : 상대의 아이템 한개를 강탈
-    
+    print("아드레날린")
+
 class Drug:                  # 약 : 50%의 확률로 회복 or 체력 -1
-    
+    print("약")
 
 bulletTable = BulletTable()
 bulletTable.generate()
