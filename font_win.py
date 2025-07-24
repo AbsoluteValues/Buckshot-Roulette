2a483f7b3ecd45117df1feb21c6d14ca34fd8b84
yuri0113 <kimbowl0113@gmail.com>
2025-07-24 15:45:02 +0900

    add mainbtn

---
+++ font_win.py
@@ -0,0 +1,16 @@
+#폰트 등록하기 윈도우 전용
+
+import ctypes
+import os
+
+def add_font(font_path):
+    FR_PRIVATE = 0x10
+    path = os.path.abspath(font_path)
+    if os.path.exists(path):
+        ctypes.windll.gdi32.AddFontResourceExW(path, FR_PRIVATE, 0)
+    else:
+        raise FileNotFoundError(f"Font file not found: {path}")
+    
+
+# 실행 시 폰트 
+add_font("Boucherie-Block.ttf")
---
+++ title.py
@@ -0,0 +1,58 @@
+from tkinter import *
+from tkinter import font
+from PIL import Image, ImageTk
+from font_win import add_font # font_win.py 에서 불러오기 
+
+add_font("Boucherie-Block.ttf")
+
+window = Tk()
+window.title("Buckshot Roulette")
+window.geometry("1280x720")
+window.iconbitmap("./image/Buckshot_Roulette.ico")
+
+#폰트설정
+custom_font = font.Font(family= "Boucherie Block", size=100) #글씨크기
+button_font = font.Font(family= "Boucherie Block", size=40) 
+
+canvas = Canvas(window, width=1280, height=720, highlightthickness=0)
+canvas.pack(fill="both", expand=True)
+
+# 배경 이미지
+wall_image = Image.open("./image/wallpaper.png").convert("RGBA").resize((1280, 720))
+wall_photo = ImageTk.PhotoImage(wall_image)
+canvas.create_image(0, 0, image=wall_photo, anchor="nw")
+
+#피 배경
+blood_image = Image.open("./image/title_blood.png").convert("RGBA").resize((1280, 720))
+blood_photo = ImageTk.PhotoImage(blood_image)
+canvas.create_image(0, 0, image=blood_photo, anchor="nw")
+
+
+#제목 텍스트 위치 
+canvas.create_text(640, 200, text="Buckshot Roulette", font=custom_font, fill="white", anchor="center")
+
+#메뉴 3개 
+def start_game():
+    print("Game Start")
+
+def mode () :
+    print("Mode")
+
+def exit_game():
+    window.destroy()
+
+#버튼 생성  텍스트, 글꼴, 실행할 함수, 버튼 가로, 외각선, 강조 테두리, 배경색, 글자색, 눌렀을때 2개 
+btn_start = Button(window, text="Game Start", font=button_font,command=start_game,width=12,
+   bd=0,highlightthickness=0,bg="black",fg="white",activebackground="gray20",activeforeground="white")
+btn_mode = Button(window, text="Mode", font=button_font, command=mode, width=10,
+    bd=0,highlightthickness=0,bg="black",fg="white",activebackground="gray20",activeforeground="white")
+btn_exit = Button(window, text="Exit", font=button_font, command=exit_game, width=10,
+    bd=0,highlightthickness=0,bg="black",fg="white",activebackground="gray20",activeforeground="white")
+
+#버튼 위치 
+btn_start.place(x=640, y=340, anchor="center")
+btn_mode.place(x=640, y=465, anchor="center")
+btn_exit.place(x=640, y=590, anchor="center")
+
+
+window.mainloop()
