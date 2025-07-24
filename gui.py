d6df5abb545623b7557382edbea00ccb69539b9f
Absolute Value <heejun1708@gmail.com>
2025-07-24 11:19:50 +0900

    fix: image transparency problem

--- gui.py
+++ gui.py
@@ -5,34 +5,24 @@ from PIL import Image, ImageTk
 window = Tk()
 window.title("Buckshot Roulette")
 window.geometry("1280x720")
+window.iconbitmap("./image/Buckshot_Roulette.ico")
 
-window.iconbitmap("./image/Buckshot_Roulette.ico") #게임 아이콘 변경 
-# 경로를 이렇게 지정하는게 맞는지 모르겟음 수정 필요 
+canvas = Canvas(window, width=1280, height=720, highlightthickness=0)
+canvas.pack(fill="both", expand=True)
 
-#배경화면 
-wall_image_path = ("./image/wallpaper.png")
-wall_image = Image.open(wall_image_path).convert("RGBA").resize((1280, 720))
-
+# 배경 이미지
+wall_image = Image.open("./image/wallpaper.png").convert("RGBA").resize((1280, 720))
 wall_photo = ImageTk.PhotoImage(wall_image)
+canvas.create_image(0, 0, image=wall_photo, anchor="nw")
 
-wall_label = Label(window, image=wall_photo)
-wall_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-#게임 플레이 화면 (게임 보드 판)
-board_image_path = ("./image/game_board.png")
-board_image = Image.open(board_image_path)
-board_image = board_image.resize((1280, 720))
+# 보드 이미지
+board_image = Image.open("./image/game_board.png").convert("RGBA").resize((1280, 720))
 board_photo = ImageTk.PhotoImage(board_image)
+canvas.create_image(0, 0, image=board_photo, anchor="nw")
 
-board_label = Label(window, image=board_photo)
-board_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-ItemBeerImg = Image.open("./image/beer.png").convert("RGBA").resize((100, 100))
-BeerImg = ImageTk.PhotoImage(ItemBeerImg)
-
-ItemBeerImg_label1 = Label(window, image = BeerImg, bd = 0)
-ItemBeerImg_label1.image = BeerImg
-ItemBeerImg_label1.pack()
-
+# 투명 PNG (Beer)
+beer_image = Image.open("./image/beer.png").convert("RGBA").resize((100, 100))
+beer_photo = ImageTk.PhotoImage(beer_image)
+canvas.create_image(100, 100, image=beer_photo, anchor="nw")
 
 window.mainloop()
